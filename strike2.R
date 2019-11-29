170422ea2c69ad3970865b20905086b5cc43b8aa
Laura Munoz Baena <Laura@biomech.local>
2019-11-29 11:31:01 -0500

    dont understand the error

--- strike2.R
+++ strike2.R
@@ -1,34 +1,31 @@
 source('country_geocoding.R')
 
 library(ggmap)
-data <- read.csv("pais.csv", sep = "|", header=T)
+data <- read.csv("Responses1.csv", sep = ",", header=T)
+country<- data$País.donde.reside.actualmente
 
 # From Google maps
-register_google(key = "AIzaSyBHXXWu667ViYsh-ZNqGcFMANacLO69ZFA")
+register_google(key = "AIzaSyArEZpq9pQRmm03tuPJQ7kpHBY5R0b_CQI")
 
 library("ggmap")
 library(maptools)
 library(maps)
 
-visited <- as.character(data$pais)
-#TODO: make the next line to return a data frame
+visited <- as.character(country)[2:5]
+
 ll.visited <- country_geocoding(visited)
 visit.x <- ll.visited$lon
 visit.y <- ll.visited$lat
 
+library(plyr)
+counts <- ddply(ll.visited, ~ country, nrow)
+uni <- unique(ll.visited)
+total <- left_join(counts, uni, by="country")
+
 #First try of a map
-#map("world", fill=TRUE, col="white", bg="lightblue", ylim=c(-60, 90), mar=c(0,0,0,0))
-#points(visit.x,visit.y, col="red", pch=16)
 mp <- NULL
 mapWorld <- borders("world", colour="gray50", fill="gray50") # create a layer of borders
 mp <- ggplot() +   mapWorld
-
 #Now Layer the cities on top
-mp <- mp+ geom_point(aes(x=visit.x, y=visit.y) ,color="blue", size=3) 
+mp <- mp+ geom_point(data = total, aes(x=lon, y=lat, size=V1), color="blue", alpha=.5) 
 mp
-
-# Second try of a map (not working)
-map <- get_map(location = 'world')
-mapPoints <- ggmap(map) + 
-          geom_point(aes(x = lon, y = lat, size = table(ll.visited$address)), data = ll.visited, alpha = .5)
-mapPoints
